1688274d7a6f93278f2c99e5ccec56c14dfee239
bijaypoudel1 <poudelbijay791@gmail.com>
2022-02-09 11:34:45 +0545

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./public/*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Circular Std', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+
+
